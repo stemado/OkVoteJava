ef95848aa0d05406f1a6c5974c95f234a2a67a47
stemado <istemado@gmail.com>
2024-04-24 05:28:08 -0500

    Add ability to download committee records excel file
    The BillExcelFileDownloader class downloads an Excel file from the Committee Votes page. Playwright library is used to automate the browser, navigate to the Committee Votes URL, locate the download button, start downloading the Excel file, and save it in the user's download folder. The suggested filename is manipulated to replace .xls extension with .html since contents of .xls file is all html if viewed in text editor.

--- src/main/java/OkVote/BillExcelFileDownloader.java
+++ src/main/java/OkVote/BillExcelFileDownloader.java
@@ -7,43 +7,77 @@ package OkVote;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-
 import com.microsoft.playwright.*;
+
 /**
  *
  * @author sdoherty
  * https://playwright.dev/java/docs/browsers#install-browsers
+ * https://playwright.dev/java/docs/downloads#introduction
  * https://chat.openai.com/share/e/960a7028-afa0-496c-ab0f-b1439f05ab09
  */
 public class BillExcelFileDownloader {
+    public String CommitteeVotesUrl = "https://former.okhouse.gov/Committees/ShowVotes.aspx";
+    private String ExcelFileDownloadButtonId = "button#ctl00_ContentPlaceHolder1_RadGrid1_ctl00_ctl02_ctl00_ExportToExcelButton";
+
+   
     public ArrayList<String> GetCommitteeRecords()
     {
-        try (Playwright playwright = Playwright.create()) {
-            
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
-            Page page = browser.newPage();
-            page.navigate("http://playwright.dev");
-            // Wait for the download to start
-            Download download = page.waitForDownload(() -> {
-                // Perform the action that initiates download
-                page.getByText("Download file").click();
-            });
-            
-            // Get user's downloads folder path
-            String downloadsFolderPath = GetUserDirectoryDownloadsFolderPath();
 
+         String filePath = DownloadCommitteeRecordsFile();
+    
+         // Default until I finish logic for parsing the file.
+        return new ArrayList<String>();
+    }
+
+    private String DownloadCommitteeRecordsFile() {
+        String suggestedFilename = "";
+        
+        try (Playwright playwright = Playwright.create()) {
+            // Set-up Playwright to use Chromium and setHeadless to false which allows us to watch the browser automation operate
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
+            
+            // Open a new Chrome browser page
+            Page page = browser.newPage();
+            
+            // Navigate to the Committee Votes page
+            page.navigate(CommitteeVotesUrl);
+
+            // Locate the Excel file download button on the page
+            Locator button = page.locator(ExcelFileDownloadButtonId);
+            
+            // Wait for the button to be ready and visible on the page
+            button.waitFor();
+
+            // Wait for the download to start after clicking the Excel button
+            Download download = page.waitForDownload(() -> {
+                // click the Excel link to download the file
+                button.click();
+                
+            });
+
+            // Get user's downloads folder path to save the file to
+            // Call GetUserDirectoryDownloadsFolderPath() from constructor
+            
+            String downloadsFolderPath = GetUserDirectoryDownloadsFolderPath();
+            
+            // Get suggested file name which is the name of the file being downloaded
+            suggestedFilename = download.suggestedFilename();
+            
+            // Change suggestedFileName extension from .xls to .html
+            suggestedFilename = suggestedFilename.replace(".xls", ".html");
+            
             // Wait for the download process to complete and save the downloaded file somewhere
-            download.saveAs(Paths.get(downloadsFolderPath, download.suggestedFilename()));
-            System.out.println(page.title());
+            download.saveAs(Paths.get(downloadsFolderPath, suggestedFilename));
         }
         catch (Exception e)
         {
             System.out.println(e.getMessage());
         }
-        
-        return new ArrayList<>();
+
+        return suggestedFilename;
     }
-    
+
     private String GetUserDirectoryDownloadsFolderPath()
     {
         // Get the user's home directory
