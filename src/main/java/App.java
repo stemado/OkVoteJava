9c0ec846eb735c2f2c3ba02b21075513ca68f983
stemado <istemado@gmail.com>
2024-04-24 04:48:53 -0500

    Set App.java to run BillExcelFileDownloader

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,6 @@
+
+import OkVote.BillExcelFileDownloader;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,6 +13,14 @@
 public class App {
     public static void main(String[] args)
     {
-        // Testing github connection
+        try {
+            // Stephen Doherty's Code
+            BillExcelFileDownloader billExcelFileDownloader = new BillExcelFileDownloader();
+            billExcelFileDownloader.GetCommitteeRecords();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 }
